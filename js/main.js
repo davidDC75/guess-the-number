70269bc751ffbd48232fa89f0a9e5b1e4fbcfbb9
David De Castro <segphault@gmail.com>
2023-04-22 00:23:08 +0200

    Correctif main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let guesses = document.querySelector('.guesses');
 let lastResult = document.querySelector('.lastResult');
 let lowOrHi = document.querySelector('.lowOrHi');
 
-let guessSubmit = document.querySelector('.quessSubmit');
+let guessSubmit = document.querySelector('.guessSubmit');
 let guessField = document.querySelector('.guessField');
 
 let guessCount = 1;
